d09203526496ad1a817fdebc52a0fc6396436d05
GanYihuan <583520052@qq.com>
2018-06-24 17:18:17 +0800

    add file

---
+++ chapter9/postcss.config.js
@@ -0,0 +1,20 @@
+﻿// 添加前缀
+const autoprefixer = require('autoprefixer');
+// 合并css文件
+const atImport = require('postcss-import')
+// 压缩代码
+const cssnano = require('cssnano');
+// 
+const cssnext = require('cssnext');
+
+module.exports = {
+  plugins: [
+    atImport,
+    autoprefixer({
+      // browsers: ['> 1%']
+      browsers: ['last 2 versions']
+    }),
+    // 放置最后
+    cssnano
+  ]
+}
